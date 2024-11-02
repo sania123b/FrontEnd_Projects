cf0fa934d326d17a6ffa72492fc41b4eae6af697
SANIA <159150400+sania123b@users.noreply.github.com>
2024-11-02 23:46:50 +0530

    Create index.js

---
+++ Speech to Text/index.js
@@ -0,0 +1,19 @@
+const results = document.getElementById("result");  
+const speechRecognition = window.speechRecognition || window.webkitSpeechRecognition;  
+const recognition = new speechRecognition();  
+recognition.onstart = function(){  
+}  
+recognition.onresult = function(event){  
+  const text = event.results[0][0].transcript;  
+//   console.log(text);  
+  document.getElementById("result").innerHTML = text;  
+}  
+function copyDivToClipboard() {  
+  const range = document.createRange();  
+  range.selectNode(document.getElementById("result"));  
+  window.getSelection().removeAllRanges(); // clear current selection  
+  window.getSelection().addRange(range); // to select text  
+  document.execCommand("copy");  
+  window.getSelection().removeAllRanges();// to deselect  
+  alert("Copied the text:")  
+}  
